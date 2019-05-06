607f07699d6d8fd0f96a1bbb07c8db56e2641cce
Jean Velonis <jean.velonis@gmail.com>
2019-05-06 01:24:59 +0000

    current user

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @places = Place.all
@@ -20,6 +20,10 @@ end
 
 def edit
     @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
