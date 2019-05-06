f38a1feda799a3193e9eaeba558298132aff49aa
Jean Velonis <jean.velonis@gmail.com>
2019-05-06 02:59:56 +0000

    geo steps

--- Gemfile
+++ Gemfile
@@ -72,3 +72,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "font-awesome-rails"
 gem 'devise'
+gem "geocoder"
+gem "figaro"
+
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true, length: { minimum: 3 }
-  validates :address, presence: true
-  validates :description, presence: true
+  geocoded_by :address
+  after_vlidation :geocode 
+
+validates :name, :address, :description, presence: true, length: { minimum: 3 }
 end
