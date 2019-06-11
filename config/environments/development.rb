0c89504a2f728a391c6a4265d5f1444ce29cb533
Jean Velonis <jean.velonis@gmail.com>
2019-06-11 23:05:16 +0000

    update to emailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "example.com"
+      domain: "example.com",
       authentication: "plan",
       enable_starttls_auto: true,
       user_name: ENV['GMAIL_ADDRESS'],
